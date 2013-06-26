0dc5b3dbfb7186e3546bd2a13f362409b7c5a201
Jure Cuhalev <gandalfar@gmail.com>
2013-06-26 16:18:38 +0200

    logo order

--- logos.php
+++ logos.php
@@ -2,10 +2,10 @@
 	<div class="small-11 small-centered columns">
 		<ul class="small-block-grid-6">
 			<?php $dir = get_stylesheet_directory_uri().'/images'; ?>
-			<li><a href="http://www.k4all.org/" target="_blank"><img src="<?php echo $dir; ?>/logo-k4a.gif" /></a></li>
-			<li><a href="http://mediamixer.eu/" target="_blank"><img src="<?php echo $dir; ?>/logo-mm.gif" /></a></li>
 			<li><a href="http://www.ocwconsortium.org/" target="_blank"><img src="<?php echo $dir; ?>/logo-ocwc.gif" /></a></li>
+			<li><a href="http://www.k4all.org/" target="_blank"><img src="<?php echo $dir; ?>/logo-k4a.gif" /></a></li>
 			<li><a href="http://www.translectures.eu/" target="_blank"><img src="<?php echo $dir; ?>/logo-transectures.gif" /></a></li>
+			<li><a href="http://mediamixer.eu/" target="_blank"><img src="<?php echo $dir; ?>/logo-mm.gif" /></a></li>
 			<li><a href="http://videolectures.net/" target="_blank"><img src="<?php echo $dir; ?>/logo-vidlec.gif" /></a></li>
 			<li><a href="http://www.xlike.org/" target="_blank"><img src="<?php echo $dir; ?>/logo-xlike.gif" /></a></li>
 		</ul>
