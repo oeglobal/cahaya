bdc2b3e0500d6283080efcc69a307b7fd3a32777
Jure Cuhalev <gandalfar@gmail.com>
2013-06-26 16:40:54 +0200

    google analytics

--- footer.php
+++ footer.php
@@ -6,5 +6,17 @@
 
 	<script src="<?php echo get_stylesheet_directory_uri(); ?>/lib/javascripts/plugins/responsiveslides.min.js"></script>
 	<script src="<?php echo get_stylesheet_directory_uri(); ?>/lib/javascripts/script.js"></script>
+
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-4248822-4']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>	
 </body>
 </html>
