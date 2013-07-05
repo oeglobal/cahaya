5c2c36d02fee04563baa5312557b40f677003542
Jure Cuhalev <gandalfar@gmail.com>
2013-07-05 08:26:43 +0200

    added JSI logo

--- logos.php
+++ logos.php
@@ -1,9 +1,10 @@
 <div class="row collapse logos white">
 	<div class="small-11 small-centered columns">
-		<ul class="small-block-grid-6">
+		<ul class="small-block-grid-7">
 			<?php $dir = get_stylesheet_directory_uri().'/images'; ?>
 			<li class="bottomlogo"><a href="http://www.ocwconsortium.org/" target="_blank"><img src="<?php echo $dir; ?>/logo-ocwc.gif" /></a></li>
 			<li class="bottomlogo"><a href="http://www.k4all.org/" target="_blank"><img src="<?php echo $dir; ?>/logo-k4a.gif" /></a></li>
+			<li class="bottomlogo"><a href="http://www.ijs.si/" target="_blank"><img src="<?php echo $dir; ?>/logo-ijs.gif" /></a></li>
 			<li class="bottomlogo"><a href="http://www.translectures.eu/" target="_blank"><img src="<?php echo $dir; ?>/logo-transectures.gif" /></a></li>
 			<li class="bottomlogo"><a href="http://mediamixer.eu/" target="_blank"><img src="<?php echo $dir; ?>/logo-mm.gif" /></a></li>
 			<li class="bottomlogo"><a href="http://videolectures.net/" target="_blank"><img src="<?php echo $dir; ?>/logo-vidlec.gif" /></a></li>
