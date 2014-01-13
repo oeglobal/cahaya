4b8e8adbc59bcf6c997de176e8d302b13f0bb4a7
Jure Cuhalev <gandalfar@gmail.com>
2014-01-13 03:37:40 -0800

    add title to pages

--- functions.php
+++ functions.php
@@ -85,6 +85,20 @@ function register_cahaya_slideshow() {
 	);
 }
 
+function filter_wp_title( $title ) {
+	global $page, $paged;
 
+	if ( is_feed() )
+		return $title;
+
+	$site_description = get_bloginfo( 'description' );
+
+	$filtered_title = $title . get_bloginfo( 'name' );
+	$filtered_title .= ( ! empty( $site_description ) && ( is_home() || is_front_page() ) ) ? ' | ' . $site_description: '';
+	$filtered_title .= ( 2 <= $paged || 2 <= $page ) ? ' | ' . sprintf( __( 'Page %s' ), max( $paged, $page ) ) : '';
+
+	return $filtered_title;
+}
+add_filter( 'wp_title', 'filter_wp_title' );
 
 ?>
