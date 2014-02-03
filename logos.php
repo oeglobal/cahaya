2e33f663d28aa665b453f1b2784181b348fd7d1c
Jure Cuhalev <gandalfar@gmail.com>
2014-02-03 17:00:29 +0100

    added OE logo

--- logos.php
+++ logos.php
@@ -1,6 +1,6 @@
 
 	<div class="small-12 small-centered">
-		<ul class="small-block-grid-7 bottomlogo" >
+		<ul class="small-block-grid-8 bottomlogo" >
 			<?php $dir = get_stylesheet_directory_uri().'/images'; ?>
 			<li><a href="http://www.ocwconsortium.org/" target="_blank"><img src="<?php echo $dir; ?>/logo-ocwc.gif" /></a></li>
 			<li><a href="http://www.k4all.org/" target="_blank"><img src="<?php echo $dir; ?>/logo-k4a.gif" /></a></li>
@@ -9,5 +9,6 @@
 			<li><a href="http://mediamixer.eu/" target="_blank"><img src="<?php echo $dir; ?>/logo-mm.gif" /></a></li>
 			<li><a href="http://videolectures.net/" target="_blank"><img src="<?php echo $dir; ?>/logo-vidlec.gif" /></a></li>
 			<li><a href="http://www.xlike.org/" target="_blank"><img src="<?php echo $dir; ?>/logo-xlike.gif" /></a></li>
+			<li><a href="http://openeducationeuropa.eu/" target="_blank"><img src="<?php echo $dir; ?>/logo-oe.png" /></a></li>
 		</ul>
 	</div>
