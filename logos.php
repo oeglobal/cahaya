42d54b34fb790e76c91f65534fc5ca3597fdd35f
Jure Cuhalev <gandalfar@gmail.com>
2014-02-20 13:28:45 +0100

    update sponsors list

--- logos.php
+++ logos.php
@@ -6,41 +6,28 @@
 			<a href="http://www.posta.si" target="_blank"><img src="<?php echo $dir; ?>/logo-posta.png" /></a><br />
 			<a href="http://videolectures.net/" target="_blank"><img src="<?php echo $dir; ?>/logo-vidlec.gif" /></a>
 		</p>
+		<h3>Patronage</h3>
+		<p><a href="http://www.up-rs.si/eng/"><img src="<?php echo $dir; ?>/logo-prs.png" /></a></p>
+
+		<h3>Support</h3>
+		<p><a href="http://www.mizs.gov.si/en/" target="_blank"><img src="<?php echo $dir; ?>/logo-mizs.jpg" /></a></p>		
 	</div>
 	<div class="small-4 columns">
 		<h3>Silver sponsors</h3>
 		<p>
 			<a href="http://www.translectures.eu/" target="_blank"><img src="<?php echo $dir; ?>/logo-transectures.gif" /></a><br />
 			<a href="http://www.xlike.org/" target="_blank"><img src="<?php echo $dir; ?>/logo-xlike.gif" /></a>
-		</p>
-	</div>
-	<div class="small-4 columns">
-		<h3>Bronze sponsor</h3>
-		<p>
 			<a href="http://mediamixer.eu/" target="_blank"><img src="<?php echo $dir; ?>/logo-mm.gif" /></a>
 		</p>
-	</div>
-</div>
-<div class="row">
-	<div class="small-4 columns">
-		<h3>Patronage</h3>
-		<p><a href="http://www.up-rs.si/eng/"><img src="<?php echo $dir; ?>/logo-prs.png" /></a></p>
-
-		<h3>Support</h3>
-		<p>
-			<a href="http://www.mizs.gov.si/en/" target="_blank"><img src="<?php echo $dir; ?>/logo-mizs.jpg" /></a>
-		</p>
-	</div>
-	<div class="small-4 columns pull-left">
 		<h3>Media sponsor</h3>
 		<p>
 			<a href="http://openeducationeuropa.eu/" target="_blank"><img src="<?php echo $dir; ?>/logo-oe.png" /></a>
-		</p>
+		</p>		
 	</div>
 	<div class="small-4 columns">
 		<h3>Organized by</h3>
 		<p><a href="http://www.ocwconsortium.org/" target="_blank"><img src="<?php echo $dir; ?>/logo-ocwc.gif" /></a></p>
 		<p><a href="http://www.k4all.org/" target="_blank"><img src="<?php echo $dir; ?>/logo-k4a.gif" /></a><br /></p>
 		<p><a href="http://www.ijs.si/" target="_blank"><img src="<?php echo $dir; ?>/logo-ijs.gif" /></a></p>
-	</div>
+	</div>	
 </div>
