a89638091d16e0848d6053d44acdfb0b11bf8f47
Jure Cuhalev <gandalfar@gmail.com>
2014-02-24 21:38:36 +0100

    update sponsors

--- logos.php
+++ logos.php
@@ -1,16 +1,16 @@
 <?php $dir = get_stylesheet_directory_uri().'/images'; ?>
 <div class="row">
 	<div class="small-4 columns">
+		<h3>Platinum sponsor</h3>
+		<p>
+			<a href="http://www.posta.si" target="_blank"><img src="<?php echo $dir; ?>/logo-posta.png" /></a>
+		</p>
+	</div>
+	<div class="small-4 columns">
 		<h3>Gold sponsor</h3>
 		<p>
-			<a href="http://www.posta.si" target="_blank"><img src="<?php echo $dir; ?>/logo-posta.png" /></a><br />
 			<a href="http://videolectures.net/" target="_blank"><img src="<?php echo $dir; ?>/logo-vidlec.gif" /></a>
 		</p>
-		<h3>Patronage</h3>
-		<p><a href="http://www.up-rs.si/eng/"><img src="<?php echo $dir; ?>/logo-prs.png" /></a></p>
-
-		<h3>Support</h3>
-		<p><a href="http://www.mizs.gov.si/en/" target="_blank"><img src="<?php echo $dir; ?>/logo-mizs.jpg" /></a></p>		
 	</div>
 	<div class="small-4 columns">
 		<h3>Silver sponsors</h3>
@@ -19,11 +19,21 @@
 			<a href="http://www.xlike.org/" target="_blank"><img src="<?php echo $dir; ?>/logo-xlike.gif" /></a>
 			<a href="http://mediamixer.eu/" target="_blank"><img src="<?php echo $dir; ?>/logo-mm.gif" /></a>
 		</p>
+	</div>
+</div>
+<div class="row">
+	<div class="small-4 columns">
 		<h3>Media sponsor</h3>
 		<p>
 			<a href="http://openeducationeuropa.eu/" target="_blank"><img src="<?php echo $dir; ?>/logo-oe.png" /></a>
 		</p>		
 	</div>
+	<div class="small-4 columns">
+		<h3>Patronage</h3>
+		<p><a href="http://www.up-rs.si/eng/"><img src="<?php echo $dir; ?>/logo-prs.png" /></a></p>
+		<h3>Support</h3>
+		<p><a href="http://www.mizs.gov.si/en/" target="_blank"><img src="<?php echo $dir; ?>/logo-mizs.jpg" /></a></p>		
+	</div>	
 	<div class="small-4 columns">
 		<h3>Organized by</h3>
 		<p><a href="http://www.ocwconsortium.org/" target="_blank"><img src="<?php echo $dir; ?>/logo-ocwc.gif" /></a></p>
