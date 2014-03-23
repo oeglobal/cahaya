30eb17a4fbbe3e4fd4f82755841bc47aa6797a7c
Jure Cuhalev <gandalfar@gmail.com>
2014-03-23 14:36:28 +0100

    fix schedule day switching

--- partials/event_days.php
+++ partials/event_days.php
@@ -1,10 +1,22 @@
 <div class="small-8 columns">
-    <dl class="sub-nav">
-        <dd <?php if ($post->ID === 489) : ?>class="active"<?php endif; ?>><a href="/2014/schedule/">Day 1, Wednesday 23.4.</a></dd>
-        <dd <?php if ($post->ID === 661) : ?>class="active"<?php endif; ?>>Day 2, Thursday 24.4.</a></dd>
-        <dd <?php if ($post->ID === 664) : ?>class="active"<?php endif; ?>>Day 3, Friday 25.4.</a></dd>
-    </dl>
-    <dl class="sub-nav">
-    	<dd><a href="/2014/webcamp-ljubljana/">WebCamp, Saturday 26.4.</a></dd>
-    </dl>
+	<dl class="sub-nav">
+		<?php if ($post->ID === 489) : ?>
+			<dd class="active"><a href="/2014/schedule/">Day 1, Wednesday 23.4.</a></dd>
+		<?php else : ?>
+			<dd><a href="/2014/schedule/">Day 1, Wednesday 23.4.</a></dd>
+		<?php endif; ?>
+		<?php if ($post->ID === 661) : ?>
+			<dd class="active"><a href="/2014/schedule/thursday/">Day 2, Thursday 24.4.</a></dd>
+		<?php else : ?>
+			<dd><a href="/2014/schedule/thursday/">Day 2, Thursday 24.4.</a>
+		<?php endif; ?>
+		<?php if ($post->ID === 664) : ?>
+			<dd class="active"><a href="/2014/schedule/friday/">Day 3, Friday 25.4</a></dd>
+		<?php else : ?>
+			<dd><a href="/2014/schedule/friday/">Day 3, Friday 25.4</a>
+		<?php endif; ?>
+	</dl>
+	<dl class="sub-nav">
+		<dd><a href="/2014/webcamp-ljubljana/">WebCamp, Saturday 26.4.</a></dd>
+	</dl>
 </div>
