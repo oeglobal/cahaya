0a75d559964d9f103aa8fd586c276bbecfd595c7
Jure Cuhalev <gandalfar@gmail.com>
2014-04-07 16:59:46 +0200

    add go-gn link

--- partials/event_legend.php
+++ partials/event_legend.php
@@ -5,6 +5,6 @@
             <li class="pedagogical track">Pedagogical track</li>
             <li class="policy track">Policy track</li>
             <li class="dissemination track">Dissemination track</li>
-            <li class="gogn track">GO-GN track</li>
+            <li class="gogn track"><a href="/2014/ai1ec_event/go-gn-research-track/">GO-GN Research Track</a></li>
         </ul>
     </div>
