b74a71c93c82efba55b117f7c918473047ac5c8f
Jure Cuhalev <gandalfar@gmail.com>
2014-04-11 11:05:51 +0200

    webcamp

---
+++ pages/schedule-webcamp.php
@@ -0,0 +1,26 @@
+<?php
+/*
+	Template name: Schedule - WebCamp
+*/
+?>
+<?php get_header() ?>
+	<div class="row white blue-bottom">
+		<div class="small-12 columns">
+			<div class="row post">
+				<div class="small-12 columns content">
+					<?php if ( have_posts() ) : ?>
+						<?php while ( have_posts() ) : the_post(); ?>
+							<div><?php the_content(); ?></div>
+						
+							<?php get_template_part('partials/event_webcamp'); ?>
+						<?php endwhile; ?>
+					<?php else : ?>
+						<h1>404 Not found</h1>
+					<?php endif; ?>
+				</div>
+				
+			</div>
+			<?php get_template_part('logos'); ?>
+		</div>
+	</div>
+<?php get_footer() ?> 
---
+++ partials/event_webcamp.php
@@ -0,0 +1,77 @@
+<div class="row">
+    <?php get_template_part('partials/event_days'); ?>
+</div>
+
+<table class="custom-schedule">
+    <thead>
+        <tr>
+            <th class="time-col"></th>
+            <th class="room-col">FRI Room 1</th>
+            <th class="room-col">FRI Room 2</th>
+            <th class="room-col">FRI Room 3</th>
+        </tr>
+    </thead>
+    
+    <tbody>
+        <tr>
+            <td class="schedule-time">8:30</td>
+            <td colspan="3">Registration</td>
+        </tr>
+        <tr>
+        	<td class="schedule-time">8:30</td>
+        	<td colspan="3">
+        		<ul>
+					<li>Viva la scaling</li>
+					<li>Etalio, mobile ID for developers, users and telcos</li>
+					<li>Find me quickly… and easily</li>
+					<li>From development and testing, to deployment of webapps with nix</li>
+					<li>Towards functional web</li>
+					<li>Why Flash sucks and it’s not the right time for HTML5</li>
+					<li>Ljubljana vs. San Francisco</li>
+					<li>.. and many more (<a href="http://webcamp.si/talks/">see the full schedule</a>)</li>
+				</ul>
+        	</td>
+        </tr>
+        <tr class="break">
+            <td class="schedule-time">11:30</td>
+            <td colspan="3">Coffee break</td>
+        </tr>
+        <tr>
+            <td class="schedule-time">14:00</td>
+            <td colspan="3">
+	        	<ul>
+					<li>Writing maintainable and extensible CSS</li>
+					<li>The time is out of joint: O cursed spite, / That ever I was born to set it right!</li>
+					<li>Frontend translation</li>
+					<li>Integration of Online Payments</li>
+					<li>Typescript. Should you care?</li>
+					<li>IPv6: The Good, the Bad and the Ugly</li>
+					<li>Hacking Conversions</li>
+					<li>Web@Desktop</li>
+					<li>Vagrant & Puppet</li>
+					<li>.. and many more (<a href="http://webcamp.si/talks/">see the full schedule</a>)</li>
+	        	</ul>
+           </td>
+        </tr>
+        <tr class="break">
+            <td class="schedule-time">13:00</td>
+            <td colspan="3">Lunch</td>
+        </tr>
+        <tr>
+            <td class="schedule-time">14:00</td>
+            <td colspan="3">
+            	<strong>Lightning talks</strong>
+
+            	<ul>
+            		<li>Love, art and war – the PHP story</li>
+            		<li>.. and many more (<a href="http://webcamp.si/talks/">see the full schedule</a>)</li>
+
+            </td>
+        </tr>
+        <tr>
+            <td class="schedule-time">16:00</td>
+            <td colspan="3">Closing and after drinks in nearby bar</td>
+        </tr>
+    </tbody>
+</table>
+
