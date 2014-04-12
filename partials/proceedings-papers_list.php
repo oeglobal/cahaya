12d5da51308ccc405d1824a7bc9d8a1573cefefa
Jure Cuhalev <gandalfar@gmail.com>
2014-04-12 10:47:47 +0200

    show paper numbers

--- partials/proceedings-papers_list.php
+++ partials/proceedings-papers_list.php
@@ -13,6 +13,7 @@
 							'meta_key' =>  'paper_pdf',
 							'meta_value' => false,
 							'meta_compare' => '!=',
+							'orderby' => 'paper_pdf+0, paper_pdf', // http://matthewturland.com/2008/11/05/natural-ordering-in-mysql/
 						));
 ?>
 
@@ -20,6 +21,7 @@
 <?php while ( $custom_query->have_posts() ) : $custom_query->the_post(); ?>
 	<tr>
 		<?php $paper_pdf = get_field('paper_pdf'); ?>
+		<td><?php the_field('paper_number'); ?></td>
 		<td><a href="<?php echo $paper_pdf['url']; ?>">PDF</a></td>
 		<td><?php the_field('event_speaker'); ?></td>
 		<td><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></td>
