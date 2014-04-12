ff7a4e53e2493274f2972a00403e8cdaf02b013c
Jure Cuhalev <gandalfar@gmail.com>
2014-04-12 11:36:19 +0200

    hide paper numbers

--- partials/proceedings-papers_list.php
+++ partials/proceedings-papers_list.php
@@ -30,9 +30,9 @@
 <?php while ( $custom_query->have_posts() ) : $custom_query->the_post(); ?>
 	<tr>
 		<?php $paper_pdf = get_field('paper_pdf'); ?>
-		<td><?php the_field('paper_number'); ?></td>
+		<!-- <td><?php the_field('paper_number'); ?></td> -->
 		<td><a href="<?php echo $paper_pdf['url']; ?>">PDF</a></td>
-		<td><?php the_field('event_speaker'); ?></td>
+		<td class="paper-author"><?php the_field('event_speaker'); ?></td>
 		<td><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></td>
 		
 	</tr>
