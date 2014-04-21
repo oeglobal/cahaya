f7fd9404b51a82e0f4ebc9c8fbc2fe8dc0e6deed
Jure Cuhalev <gandalfar@gmail.com>
2014-04-21 18:31:31 +0200

    link tracks

--- partials/event_legend.php
+++ partials/event_legend.php
@@ -1,9 +1,9 @@
     <div class="small-3 columns">
         <!-- <strong>Legend</strong> -->
         <ul class="legend">
-            <li class="research track">Research track</li>
-            <li class="pedagogical track">Pedagogical track</li>
-            <li class="policy track">Policy track</li>
+            <li class="research track"><a href="/2014/research-track/">Research track</a></li>
+            <li class="pedagogical track"><a href="/2014/pedagogical-track/">Pedagogical track</a></li>
+            <li class="policy track"><a href="/2014/policy-track/">Policy track</a></li>
             <li class="dissemination track">Dissemination track</li>
             <li class="gogn track"><a href="/2014/ai1ec_event/go-gn-research-track/">GO-GN Research Track</a></li>
         </ul>
