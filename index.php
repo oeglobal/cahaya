9b50d725a5793ea16216c54c269cc0ce49cb42a6
Jure Cuhalev <gandalfar@gmail.com>
2014-06-25 19:06:05 +0200

    support multiple VLN videos

--- index.php
+++ index.php
@@ -11,15 +11,18 @@
 							<h1><?php the_title(); ?></h1>
 							<div><?php the_content(); ?></div>
 
-							<?php if ( get_field('videolectures_url') ) : ?>
+							<?php if ( have_rows('videolectures_urls') ) : ?>
 								<h2>Watch Video of the Presentation on VideoLectures</h2>
 
-								<div class="vln">
-									<a href="<?php the_field('videolectures_url'); ?>" class="overlay">
-										<img src="<?php echo get_stylesheet_directory_uri().'/images/play.png'; ?>" class="overlay" />
-									</a>
-									<img src="<?php the_field('videolectures_url'); ?>screenshot.jpg" class="screenshot" />
-								</div>
+								<?php while ( have_rows('videolectures_urls') ) : the_row(); ?>
+									<div class="vln">
+										<a href="<?php the_sub_field('url'); ?>" class="overlay">
+											<img src="<?php echo get_stylesheet_directory_uri().'/images/play.png'; ?>" class="overlay" />
+										</a>
+										<img src="<?php the_sub_field('url'); ?>screenshot.jpg" class="screenshot" />
+									</div>
+									<br />
+								<?php endwhile; ?>
 							<?php endif; ?>
 
 							<?php if ( get_field('slideshare_url') ) : ?>
