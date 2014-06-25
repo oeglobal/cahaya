39220b5cefac2f27702dfb0a2f2a5c5d5a0ff540
Jure Cuhalev <gandalfar@gmail.com>
2014-06-25 18:50:56 +0200

    support VLN embeds

--- index.php
+++ index.php
@@ -11,10 +11,23 @@
 							<h1><?php the_title(); ?></h1>
 							<div><?php the_content(); ?></div>
 
+							<?php if ( get_field('videolectures_url') ) : ?>
+								<h2>Watch Video of the Presentation on VideoLectures</h2>
+
+								<div class="vln">
+									<a href="<?php the_field('videolectures_url'); ?>" class="overlay">
+										<img src="<?php echo get_stylesheet_directory_uri().'/images/play.png'; ?>" class="overlay" />
+									</a>
+									<img src="<?php the_field('videolectures_url'); ?>screenshot.jpg" class="screenshot" />
+								</div>
+							<?php endif; ?>
+
 							<?php if ( get_field('slideshare_url') ) : ?>
-								<h2>Presentation</h2>
-								<?php $html = wp_oembed_get(get_field('slideshare_url')); ?>
-								<?php echo $html; ?>
+								<div>
+									<h2>Presentation</h2>
+									<?php $html = wp_oembed_get(get_field('slideshare_url')); ?>
+									<?php echo $html; ?>
+								</div>
 							<?php endif; ?>
 						<?php endwhile; ?>
 					<?php else : ?>
